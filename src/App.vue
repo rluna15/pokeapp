b7ac527ff3c430bbaae4a912f9ed116ad509fc0c
rluna <no-replay@github.com>
2023-01-06 19:21:03 -0500

    remove unused components

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script>
 import { onMounted } from '@vue/runtime-core'
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   setup() {
@@ -24,20 +23,7 @@ export default {
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
 </template>
 
 <style scoped>
