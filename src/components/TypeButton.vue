92e04895c806b0120f05e221a0bfe7a9bd90dd6d
Ramon Luna Carrillo <rluna@realclearpolitics.com>
2023-01-16 10:22:43 -0500

    added type colors

--- src/components/TypeButton.vue
+++ src/components/TypeButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <a class="type" :href="type.url">{{ type.name }}</a>
+    <a class="type" :class="type.name" :href="type.url">{{ type.name }}</a>
 </template>
 
 <script>
@@ -23,5 +23,50 @@ export default {
     border: 1px solid;
     border-radius: 5px;
     display: block;
+}
+.grass {
+    color: #78c850
+}
+.fire {
+    color: #f08030
+}
+.water {
+    color: #6890f0
+}
+.bug {
+    color: #a8b820
+}
+.normal {
+    color: #a8a878
+}
+.poison {
+    color: #a040a0
+}
+.electric {
+    color: #f8d030
+}
+.ground {
+    color: #e0c068;
+}
+.fairy {
+    color: #ee99ac;
+}
+.fighting {
+    color: #c03028;
+}
+.psychic {
+    color: #f85888;
+}
+.rock {
+    color: #b8a038;
+}
+.ghost {
+    color: #705898;
+}
+.ice {
+    color: #98d8d8;
+}
+.dragon {
+    color: #7038f8;
 }
 </style>
