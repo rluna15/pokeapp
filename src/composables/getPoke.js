15dbe2dd23dbe4413eb590fb358ecc2519c04d7e
rluna <no-replay@github.com>
2023-01-15 19:07:08 -0500

    added getpoke composable

---
+++ src/composables/getPoke.js
@@ -0,0 +1,23 @@
+import { ref } from 'vue'
+
+export function getPoke(id) {
+    const pokeInfo = ref(null)
+    const error = ref(null)
+
+    const getInfo = async () => {
+        try {
+            let data = await fetch('https://pokeapi.co/api/v2/pokemon/' + id)
+
+            if (!data.ok) {
+                throw Error("Could not fetch data")
+            }
+
+            pokeInfo.value = await data.json()
+        } catch (err) {
+            error.value = err.message
+            console.log(error.value)
+        }
+    }
+
+    return { pokeInfo, getInfo, error }
+}
