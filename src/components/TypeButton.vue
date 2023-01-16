12db6ba25600e15365280a321aa3a904a1c8eec1
rluna <no-replay@github.com>
2023-01-15 19:06:49 -0500

    building pokedetails vue

---
+++ src/components/TypeButton.vue
@@ -0,0 +1,27 @@
+<template>
+    <a class="type" :href="type.url">{{ type.name }}</a>
+</template>
+
+<script>
+export default {
+    name: 'TypeButton',
+    props: ['type'],
+    setup(props) {
+        return {};
+    },
+};
+</script>
+
+<style scoped>
+.type {
+    display: inline;
+    font-size:x-small;
+    text-align: center;
+    text-transform: uppercase;
+    padding: 0 5px;
+    margin-top: 5px;
+    border: 1px solid;
+    border-radius: 5px;
+    display: block;
+}
+</style>
