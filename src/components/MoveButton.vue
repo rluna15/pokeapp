853ff01ff658e897534f5d74a313aa6e36951b52
rluna <no-replay@github.com>
2023-01-17 23:03:27 -0500

    create the movebutton component

---
+++ src/components/MoveButton.vue
@@ -0,0 +1,19 @@
+<template>
+    <a href="">{{ move.move.name }}</a>
+</template>
+
+<script>
+import getMove from './MoveButton.vue'; 
+
+export default {
+    name: "MoveButton",
+    props: ['move'],
+    setup() {
+        // const { move, getMoveInfo, error } = getMoveInfo()
+
+        return {};
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
