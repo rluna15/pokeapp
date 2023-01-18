eede3cd77fe8f69b628f9a7b73a84da9040d07fd
rluna <no-replay@github.com>
2023-01-17 23:03:01 -0500

    finished the getMove composable

--- src/composables/getMove.js
+++ src/composables/getMove.js
@@ -1,18 +1,26 @@
 import { ref } from 'vue'
 
 export function getMove(moveUrl) {
-    const moveList = ref([])
+    const move = ref([])
     const error = ref(null)
 
-    const getMoveInfo = async() => {
-        let data = await fetch();
+    const getMoveInfo = async () => {
+        try {
+            let data = await fetch(moveUrl)
 
-        if (!data.ok){
-            throw Error("Could not fetch data")
+            if (!data.ok) {
+                throw Error("Could not fetch poke info")
+            }
+
+            move.value = await data.json()
+        } catch (err) {
+            error.value = err.message
+            console.log(error.value)
         }
+
 
         
     }
 
-    return { moveList, getMoveInfo, error}
+    return { move, getMoveInfo, error}
 }
