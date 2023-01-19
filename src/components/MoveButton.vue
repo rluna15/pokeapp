220e09f930aa4a8bf5bb70521071b06072514d14
rluna <no-replay@github.com>
2023-01-18 20:46:28 -0500

    completed getting moveinfo

--- src/components/MoveButton.vue
+++ src/components/MoveButton.vue
@@ -1,19 +1,38 @@
 <template>
-    <a href="">{{ move.move.name }}</a>
+    <a class="move-container">
+        <span class="move-name">{{ move.move.name }}</span>
+        <span v-if="moveInfo"><type-button :type="moveInfo.type"/></span>
+        <span v-if="moveInfo">{{ moveInfo.damage_class.name }}</span>
+        <span v-if="moveInfo">Power: {{ moveInfo.power }}</span>
+        <span v-if="moveInfo">PP: {{ moveInfo.pp }}</span>
+        <span v-if="moveInfo">Accuracy: {{ moveInfo.accuracy }}</span>
+    </a>
 </template>
 
 <script>
-import getMove from './MoveButton.vue'; 
+import { ref } from '@vue/reactivity';
+import { getMove } from '../composables/getMove.js'
+
+import TypeButton from './TypeButton.vue';
 
 export default {
     name: "MoveButton",
     props: ['move'],
-    setup() {
-        // const { move, getMoveInfo, error } = getMoveInfo()
+    components: { TypeButton },
+    setup(props) {
+        const { moveInfo, getMoveInfo, error } = getMove(props.move.move.url)
 
-        return {};
+        getMoveInfo()
+        return { moveInfo, error };
     },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.move-name {
+    text-transform: capitalize;
+}
+.move-container {
+    display: flex;
+}
+</style>
