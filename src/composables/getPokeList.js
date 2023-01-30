7cb3f33b2638d70eee11ae546ed3cc9fb72fd1db
rluna <no-replay@github.com>
2023-01-29 23:59:16 -0500

    updated pokelist to get the pokedex

--- src/composables/getPokeList.js
+++ src/composables/getPokeList.js
@@ -1,43 +1,18 @@
 import { ref } from 'vue'
 
-export function getPokeList(limit) {
-    const pokeList = ref([])
-    const dataHold = ref(null)
+export function getPokeList() {
+    const pokeList = ref(null)
     const error = ref(null)
 
     const getList = async () => {
         try {
-            let data = await fetch('https://pokeapi.co/api/v2/pokemon?limit=' + limit)
+            let data = await fetch('https://pokeapi.co/api/v2/pokedex/1')
 
             if (!data.ok) {
                 throw Error("Could not fetch data")
             }
 
-            dataHold.value = await data.json()
-
-            // console.log(dataHold.value.results[1].url)
-            dataHold.value.results.forEach(poke => {
-                getInfo(poke)
-            });
-
-        } catch (err) {
-            error.value = err.message
-            console.error(error.value)
-        }
-    }
-
-    const getInfo = async (poke) => {
-        try {
-            let data = await fetch(poke.url)
-
-            if (!data.ok) {
-                throw Error("Could not fetch data")
-            }
-
-            dataHold.value = await data.json()
-
-            pokeList.value.push(dataHold.value)
-
+            pokeList.value = await data.json()
         } catch (err) {
             error.value = err.message
             console.log(error.value)
