2c934d2f7e0fcf74ef10bd603b7d47dc9441de34
rluna <no-replay@github.com>
2023-02-06 23:25:52 -0500

    added new links and styled button

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,12 +3,12 @@
         <nav>
             <h1>PokeBuilder</h1>
 
-            <!-- show user email -->
-            <div>logged in as</div>
-
             <!-- for logged in users -->
             <div>
                 <router-link to="/">Home</router-link>
+                <router-link to="/pokedex">Pokedex</router-link>
+                <router-link to="/team-builder">Team Builder</router-link>
+                <router-link to="/my-team">My Team</router-link>
                 <button @click="">Logout</button>
             </div>
 
@@ -18,12 +18,14 @@
                 <router-link to="/signup">Signup</router-link>
             </div>
         </nav>
+        <!-- show user email -->
+        <div>logged in as</div>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'NavBar',
+    name: "NavBar",
     setup() {
         return {};
     },
@@ -43,7 +45,15 @@ nav a {
     margin-left: 16px;
 }
 nav button {
+    font-size: 15px;
+    padding: 0;
     margin-left: 16px;
+    border: none;
+    background: none;
+    color: hsla(160, 100%, 37%, 1);
+}
+nav button:hover {
+    background-color: hsla(160, 100%, 37%, 0.2);
 }
 nav a.router-link-exact-active {
     color: #0ec58e;
