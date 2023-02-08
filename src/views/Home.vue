e06e046da09fbb4cc153816bd16b844f872587d1
rluna <no-replay@github.com>
2023-02-07 20:18:36 -0500

    created home and pokedex view

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <router-link to="/pokedex">Pokedex</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Pokedex.vue
@@ -0,0 +1,20 @@
+<template>
+    <poke-list/>
+</template>
+
+<script>
+import PokeList from '../components/PokeList.vue';
+export default {
+    name: 'PokedexView',
+    components: { PokeList },
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
