02a87dea31be5f252fe403cc080f93fd5ddb2a8a
rluna <no-replay@github.com>
2023-02-07 21:18:29 -0500

    Added spinner to pokelist

--- src/components/PokeList.vue
+++ src/components/PokeList.vue
@@ -1,10 +1,14 @@
 <template>
-    <div class="list-container">
+    <div v-if="error">{{ error }}</div>
+    <div v-if="pokeList" class="list-container">
         <div v-for="(poke, index) in pokeList.pokemon_entries" :key="index">
             <div v-if="index < 905">
                 <poke-card :poke="poke"/>
             </div>
         </div>
+    </div>
+    <div v-else>
+        <spinner/>
     </div>
 </template>
 
