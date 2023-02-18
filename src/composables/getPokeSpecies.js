0765e1ef607038b3f864278d8e2ace8555431fbc
rluna <no-replay@github.com>
2023-02-18 17:49:31 -0500

    updated species composable

--- src/composables/getPokeSpecies.js
+++ src/composables/getPokeSpecies.js
@@ -1,12 +1,12 @@
 import { ref } from 'vue'
 
-export function getPokeSpecies() {
+export function getPokeSpecies(url) {
     const pokeSpeciesInfo = ref(null)
     const error = ref(null)
 
     const getSpecies = async () => {
         try {
-            let data = await fetch('')
+            let data = await fetch(url)
 
             if (!data.ok) {
                 throw Error('Could not fetch data')
