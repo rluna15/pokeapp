1dc767b72440a16693dec38661bda94fa1e5834e
rluna <no-replay@github.com>
2023-03-04 17:10:33 -0500

    get and show evolution chain

--- src/components/EvolutionChain.vue
+++ src/components/EvolutionChain.vue
@@ -1,11 +1,17 @@
 <template>
     <div v-if="error">{{ error }}</div>
-    <div>
-        Evolution chain: {{ evolutionChainInfo }}
+    <div v-if="evolutionChainInfo">
+        <div v-for="c in chain" :key="c.id">
+            <router-link :to="{ name: 'pokedetails', params: { id: c.id }}">
+                <img :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${c.id}.png`" :alt="c.name">
+            </router-link>
+        </div>
     </div>
 </template>
 
 <script>
+import { computed } from "@vue/reactivity";
+
 import { getEvolutionChain } from "../composables/getEvolutionChain";
 
 export default {
@@ -16,9 +22,58 @@ export default {
 
         getChain()
 
-        console.log(props.EvChain);
+        const chain = computed(() => {
+            let c = []  //main poke array
+            let p = {}
+            let id
 
-        return { evolutionChainInfo, error }
+            //first poke info
+            id = evolutionChainInfo.value.chain.species.url.split('/')
+
+            p.id = id[id.length - 2]
+            p.name = evolutionChainInfo.value.chain.species.name
+
+            c.push(p)
+            
+            if (evolutionChainInfo.value.chain.evolution_details.length > 0)
+            {
+                p.details = evolutionChainInfo.value.chain.evolution_details[0]
+            }
+
+            //get second evolution
+            if (evolutionChainInfo.value.chain.evolves_to.length > 0)
+            {
+                evolutionChainInfo.value.chain.evolves_to.forEach((ev) => {
+                    id = ev.species.url.split('/')
+
+                    p = {}
+                    p.id = id[id.length - 2]
+                    p.name = ev.species.name
+                    p.details = ev.evolution_details[0]
+
+                    c.push(p)
+
+                    //get third evolution
+                    if (ev.evolves_to.length > 0)
+                    {
+                        ev.evolves_to.forEach((ev) => {
+                            id = ev.species.url.split('/')
+
+                            p = {}
+                            p.id = id[id.length - 2]
+                            p.name = ev.species.name
+                            p.details = ev.evolution_details[0]
+                        })
+
+                        c.push(p)
+                    }
+                })
+            }
+            
+           return c
+        })
+
+        return { evolutionChainInfo, chain, error }
     }
 }
 </script>
